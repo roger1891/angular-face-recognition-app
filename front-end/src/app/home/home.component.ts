4edceb431e06a168a8a7e1e47a722dd6df9217e3
DESKTOP-AJ96G0N\laptop\Roger <rogerrainersbraun@gmail.com>
2019-10-09 15:11:58 +0800

    ngx-webcam features
    included component features:
    1. error display
    2. turn on off
    3. take picture
    4. timer
    5. invert image

--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Subject} from "rxjs/Subject";
+import {Observable} from "rxjs/Observable";
+import {WebcamImage, WebcamInitError} from 'ngx-webcam';
 
 @Component({
   selector: 'app-home',
@@ -11,5 +14,46 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
   }
+  public showWebcam = true;
+  public mirrorImage: string = "never";
+  public errors: WebcamInitError[] = [];
+  public seconds:number ;
+  private trigger: Subject<void> = new Subject<void>();
+
+  // latest snapshot
+  public webcamImage: WebcamImage = null;
+  
+  public triggerSnapshot(): void {
+    this.seconds = 3;
+    setTimeout(()=>{
+      this.seconds = 2;
+      setTimeout(()=>{
+        this.seconds = 1
+        setTimeout(()=>{
+          this.trigger.next(); 
+          this.seconds = null;
+        },2000)
+      },2000) 
+    },2000)     
+  }
+
+ public handleImage(webcamImage: WebcamImage): void {
+  +
+   console.info("received webcam image", webcamImage);
+   this.webcamImage = webcamImage;
+ }
+
+ public get triggerObservable(): Observable<void> {
+   return this.trigger.asObservable();
+ }
+
+ //turn camera on or off
+ public startWebcam() {
+  return this.showWebcam = !this.showWebcam;
+ }
+
+ public handleInitError(error: WebcamInitError): void {
+  this.errors.push(error);
+ }
 
 }
