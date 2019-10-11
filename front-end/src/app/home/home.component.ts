57241cee2f8bf2f449240af437ea030269d4d8a8
DESKTOP-AJ96G0N\laptop\Roger <rogerrainersbraun@gmail.com>
2019-10-11 21:41:37 +0800

    added the ff. to home.component file
    1. modal imports
    2. TemplateRef, ViewChild
    3. Ngx bootsrap modal
    4. initialization for modal reference  to be called
    5. declared variables to be used in modals
    6. subscribed to image fucntion
     a. initialized variables to be used in html file
     b. made a search through database to search for name via id
     c. open modal

--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, TemplateRef, OnInit, ViewChild } from '@angular/core';
 import {Subject} from "rxjs/Subject";
 import {Observable} from "rxjs/Observable";
 import {WebcamImage, WebcamInitError} from 'ngx-webcam';
 import { FacedetAPIService } from '../facedet-api.service';
+import { ProductsService } from '../products.service';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 @Component({
   selector: 'app-home',
@@ -10,8 +13,9 @@ import { FacedetAPIService } from '../facedet-api.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private fda: FacedetAPIService) { }
+  //get #modal_Template  reference
+  @ViewChild('modal_Template', {static: false}) modalTemp: TemplateRef<void>;
+  constructor(private fda: FacedetAPIService, private ps: ProductsService, private modalService: BsModalService) { }
 
   ngOnInit() {
   }
@@ -21,9 +25,24 @@ export class HomeComponent implements OnInit {
   public seconds:number ;
   private trigger: Subject<void> = new Subject<void>();
 
+
+  //modal
+  modalRef: BsModalRef;
+  //modal content
+  private status: string
+  private confidenceLevel: number
+  private name: string
   // latest snapshot
   public webcamImage: WebcamImage = null;
   
+
+  //modal
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template,{ backdrop: 'static', keyboard: false });
+  }
+
+
+  //image
   public triggerSnapshot(): void {
     this.seconds = 3;
     setTimeout(()=>{
@@ -43,7 +62,30 @@ export class HomeComponent implements OnInit {
     this.webcamImage = webcamImage;
     //console.log("this is: " + this.webcamImage.imageAsDataUrl);
     //this.fda.sendImage(this.webcamImage.imageAsDataUrl);
-    this.fda.sendImage("http://localhost:4000/uploads/1570563000257-arnold4.jpg");
+    //send image to api
+    this.fda.sendImage("http://localhost:4000/uploads/1570563000257-arnold4.jpg").subscribe(res => {
+      //set variables
+      let entryId = res["photos"][0].tags[0].uids[0].prediction; 
+      this.status = res["status"];
+      this.confidenceLevel = (res["photos"][0].tags[0].uids[0].confidence * 100);
+      //console.log("this is res: " + JSON.stringify(res));
+      //console.log('image sent to api done');
+      //console.log("this is id: " + entryId);
+      
+
+      //check database for id
+      this.ps.editProduct(entryId).subscribe(res => {
+        //set variables
+        this.name = res["ProductName"];
+        this.openModal(this.modalTemp);
+        //alert(status + ", Name: " + name + " We are about " + confidenceLevel + "sure" );
+        //set product array to respond from url
+        //this.product = res;
+        //set default image from database
+        //this.imgURL = this.product.ProductLink;
+      })
+
+    });
   }
 
   public get triggerObservable(): Observable<void> {
