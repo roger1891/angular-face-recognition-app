2883a5824c214c542ff50054fa3cf72dc6c2436b
DESKTOP-AJ96G0N\laptop\Roger <rogerrainersbraun@gmail.com>
2019-10-14 17:22:39 +0800

    created async function
    created async function so that program gives time for br image being stored on server before looking for image and sending it to api. Otherwise, it would throw an error that no image was received

--- back-end/routes/facedetapi.route.js
+++ back-end/routes/facedetapi.route.js
@@ -5,11 +5,49 @@ const app = express();
 const facedetAPIRoutes = express.Router();
 const fs = require('fs');
 const faceDetAPI = require('../apis/FacedetAPI.js');
+const base64ToImage = require('base64-to-image');
 
 // Require Product model in our routes module
 let Product = require('../models/Product');
 let myFaceDetAPI = new faceDetAPI();
+const DIR = 'uploads/';
 
+
+
+
+function createImage(imageUrl){
+	let base64Str = imageUrl;
+	let path ='temp/';
+	let date = Date.now();
+	let imageFileName = "webcam_" + date;
+	let optionalObj = {'fileName': imageFileName, 'type':'jpg'};	
+	//creates image
+	let imageInfo = base64ToImage(base64Str,path,optionalObj); 
+
+	//base64ToImage(base64Str,path,optionalObj);
+	return imageInfo;
+}
+
+
+async function customAsyncFunc(imageName, res){
+   console.log(1);
+   await delay(1000);
+    //call promise
+   	myFaceDetAPI.recognizeImg(imageName).then(function(result) { 
+	 // here is your response back
+	 console.log("my result: " + result);
+	 res.json(result);
+	});
+   console.log(2);
+   console.log(3);
+   console.log(4);
+}
+
+function delay(ms){
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
+}
 
 //view album from apis/FacedetAPI
 facedetAPIRoutes.route("/albums").get(function (req, res){
@@ -18,15 +56,10 @@ facedetAPIRoutes.route("/albums").get(function (req, res){
 
 facedetAPIRoutes.route("/").post(function (req, res){
 	let imageUrl = req.body.url;
-	console.log("test: " + JSON.stringify(imageUrl));
-	//myFaceDetAPI.recognizeImg(imageUrl);
-	//res.status(200).json(myFaceDetAPI.recognizeImg(imageUrl));
-	//call promise
-	myFaceDetAPI.recognizeImg(imageUrl).then(function(result) { 
-	 // here is your response back
-	 console.log(result);
-	 res.json(result);
-	});
+	let imageName = createImage(imageUrl).fileName;
+	//call async function
+	customAsyncFunc(imageName, res);
+
 });
 
 module.exports = facedetAPIRoutes;
